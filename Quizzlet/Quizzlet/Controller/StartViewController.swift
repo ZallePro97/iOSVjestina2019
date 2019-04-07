c8c3ee2c7fe4cb9a63af034e1aca2846720fc4f8
Zeljko Halle <spajdi35@gmail.com>
2019-04-07 14:58:31 +0200

    finished

--- Quizzlet/Quizzlet/Controller/StartViewController.swift
+++ Quizzlet/Quizzlet/Controller/StartViewController.swift
@@ -14,6 +14,7 @@ class StartViewController: UIViewController {
     
     var question: Question?
     var correctAnswer: String?
+    var wantedColor: UIColor?
     
     @IBOutlet weak var funFactLabel: UILabel!
     @IBOutlet weak var quizzTitle: UILabel!
@@ -24,18 +25,14 @@ class StartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        wantedColor = questionView.btnA.backgroundColor
         // Do any additional setup after loading the view, typically from a nib.
     }
     
 
     @IBAction func getQuizzes(_ sender: UIButton) {
         
-        self.questionView.subviews.forEach { (subview) in
-            
-            // moram napravit refreshanje gumbi
-            
-            subview.backgroundColor = questionView.backgroundColor
-        }
+        let answerButtons = [self.questionView.btnA, self.questionView.btnB, self.questionView.btnC, self.questionView.btnD]
         
         let urlString = "https://iosquiz.herokuapp.com/api/quizzes"
         
@@ -63,50 +60,59 @@ class StartViewController: UIViewController {
                     
                     self.funFactLabel.text = "Ima \(counter) pitanja koja sadrze 'NBA'"
                 }
-                    
+                
+                //  dohvacanje neuspjesno
+                
                 if self.quizzes.count == 0 {
                     let alert = UIAlertController(title: "Alert", message: "Cannot connect to server", preferredStyle:UIAlertController.Style.alert)
                     alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
                     self.present(alert, animated: true, completion: nil)
+                } else {
+                    
+                    // dohvacanje uspjesno
+                    
+                    let randomQuizz = self.pickRandomQuizz()
+                    
+                    if let title = randomQuizz.title {
+                        self.quizzTitle.text = title
+                        self.quizzTitle.backgroundColor = randomQuizz.category?.value
+                    }
+                    
+                    if let quizzImage = randomQuizz.image {
+                        self.quizzImage.image = quizzImage
+                        self.quizzImage.backgroundColor = randomQuizz.category?.value
+                    }
+                    
+                    let question = randomQuizz.questions[Int.random(in: 0 ... randomQuizz.questions.count - 1)]
+                    print(question.question!)
+                    
+                    self.question = question
+                    self.correctAnswer = question.answers[question.correctAnswer!]
+                    
+                    if let questionText = question.question {
+                        self.questionView.questionText.adjustsFontSizeToFitWidth = true
+                        self.questionView.questionText.lineBreakMode = .byWordWrapping
+                        self.questionView.questionText.numberOfLines = 0
+                        
+                        UIView.transition(with: self.questionView.questionText, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                            self.questionView.questionText.text = questionText
+                        }, completion: nil)
+                        //
+                    }
+                    
+                    for (index, element) in answerButtons.enumerated() {
+                        UIView.animate(withDuration: 0.5, animations: {
+                            element?.backgroundColor = self.wantedColor
+                        }, completion: nil)
+                        
+                        element?.backgroundColor = self.wantedColor
+                        element?.titleLabel?.adjustsFontSizeToFitWidth = true
+                        element?.setTitle(question.answers[index], for: .normal)
+                        element?.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+                    }
                 }
                 
-                let randomQuizz = self.pickRandomQuizz()
                 
-                if let title = randomQuizz.title {
-                    self.quizzTitle.text = title
-                    self.quizzTitle.backgroundColor = randomQuizz.category?.value
-                }
-                
-                if let quizzImage = randomQuizz.image {
-                    self.quizzImage.image = quizzImage
-                    self.quizzImage.backgroundColor = randomQuizz.category?.value
-                }
-                
-                let question = randomQuizz.questions[Int.random(in: 0 ... randomQuizz.questions.count - 1)]
-                print(question.question!)
-                
-                self.question = question
-                self.correctAnswer = question.answers[question.correctAnswer!]
-                
-                if let questionText = question.question {
-                    self.questionView.questionText.text = questionText
-                }
-                
-                self.questionView.btnA.titleLabel?.adjustsFontSizeToFitWidth = true
-                self.questionView.btnA.setTitle(question.answers[0], for: .normal)
-                self.questionView.btnA.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
-                
-                self.questionView.btnB.titleLabel?.adjustsFontSizeToFitWidth = true
-                self.questionView.btnB.setTitle(question.answers[1], for: .normal)
-                self.questionView.btnB.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
-                
-                self.questionView.btnC.titleLabel?.adjustsFontSizeToFitWidth = true
-                self.questionView.btnC.setTitle(question.answers[2], for: .normal)
-                self.questionView.btnC.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
-                
-                self.questionView.btnD.titleLabel?.adjustsFontSizeToFitWidth = true
-                self.questionView.btnD.setTitle(question.answers[3], for: .normal)
-                self.questionView.btnD.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
                 
             }
 
